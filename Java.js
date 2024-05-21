4f5c135eb913356b00df4ce1c1908e750b724f97
jerevinrodelas <bumalikk0@gmail.com>
2024-05-21 16:15:53 +0800

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,9 @@
+function input1(){
+    document.getElementById("label1").style.color= "red";
+ }
+    function input2(){
+        document.getElementById("label2").style.color= "red";
+ }
+    function input3(){
+        document.getElementById("label3").style.color= "Green";
+ }
